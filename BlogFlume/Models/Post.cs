d7292a6ad15ff13de4dfb4949385bd5dee958bf4
Jonathan Mendoza <quartet4@mac.com>
2023-06-09 11:03:51 -0400

    Add Post model

---
+++ BlogFlume/Models/Post.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogFlume.Models;
+
+public class Post
+{
+    public Post(int id, int blogId, string authorId, string title, string @abstract, string content, DateTime created, DateTime? updated, bool isReady, string slug, byte[] imageData, string contentType, IFormFile image)
+    {
+        Id = id;
+        BlogId = blogId;
+        AuthorId = authorId;
+        Title = title;
+        Abstract = @abstract;
+        Content = content;
+        Created = created;
+        Updated = updated;
+        IsReady = isReady;
+        Slug = slug;
+        ImageData = imageData;
+        ContentType = contentType;
+        Image = image;
+    }
+
+    public int Id { get; set; }
+    public int BlogId { get; set; }
+    public string AuthorId { get; set; }
+
+    [Required]
+    [StringLength(75, ErrorMessage = "The {0} must be between {2} and {1} characters.", MinimumLength = 2)]
+    public string Title { get; set; }
+
+    [Required]
+    [StringLength(200, ErrorMessage = "The {0} must be between {2} and {1} characters.", MinimumLength = 2)]
+    public string Abstract { get; set; }
+
+    [Required]
+    public string Content { get; set; }
+
+    [DataType(DataType.Date)]
+    [Display(Name = "Created Date")]
+    public DateTime Created { get; set; }
+    
+    [DataType(DataType.Date)]
+    [Display(Name = "Updated Date")]
+    public DateTime? Updated { get; set; }
+
+    public bool IsReady { get; set; }
+
+    public string Slug { get; set; }
+
+    public byte[] ImageData { get; set; }
+    public string ContentType { get; set; }
+    
+    [NotMapped]
+    public IFormFile Image { get; set; }
+}
