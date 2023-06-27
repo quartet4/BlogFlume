acd0731d63283758c13ed3915cdeaf5e973522c2
Jonathan Mendoza <quartet4@mac.com>
2023-06-26 22:20:49 -0400

    Update blog constructor and model

---
+++ BlogFlume/Migrations/20230621152835_Update Blog.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogFlume.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBlog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blog_AspNetUsers_AuthorId",
+                table: "Blog");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ImageData",
+                table: "Blog",
+                type: "bytea",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Blog",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContentType",
+                table: "Blog",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                table: "Blog",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blog_AspNetUsers_AuthorId",
+                table: "Blog",
+                column: "AuthorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blog_AspNetUsers_AuthorId",
+                table: "Blog");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ImageData",
+                table: "Blog",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "bytea",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Blog",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ContentType",
+                table: "Blog",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                table: "Blog",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blog_AspNetUsers_AuthorId",
+                table: "Blog",
+                column: "AuthorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- BlogFlume/Models/Blog.cs
+++ BlogFlume/Models/Blog.cs
@@ -5,6 +5,20 @@ namespace BlogFlume.Models;
 
 public class Blog
 {
+
+    public Blog()
+    {
+        
+    }
+    
+    public Blog(string name, string description, byte[] imageData, string contentType)
+    {
+        Name = name;
+        Description = description;
+        ImageData = imageData;
+        ContentType = contentType;
+    }
+    
     public Blog(int id, string authorId, string name, string description, DateTime created, DateTime? updated, byte[] imageData, string contentType)
     {
         Id = id;
@@ -18,7 +32,7 @@ public class Blog
     }
 
     public int Id { get; set; }
-    public string AuthorId { get; set; }
+    public string? AuthorId { get; set; }
 
     [Required]
     [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters.", MinimumLength = 2)]
@@ -30,22 +44,22 @@ public class Blog
 
     [DataType(DataType.Date)]
     [Display(Name = "Created Date")]
-    public DateTime Created { get; set; }
+    public DateTime? Created { get; set; }
     
     [DataType(DataType.Date)]
     [Display(Name = "Updated Date")]
     public DateTime? Updated { get; set; }
 
     [Display(Name = "Blog Image")]
-    public byte[] ImageData { get; set; }
+    public byte[]? ImageData { get; set; }
     
     [Display(Name = "Image Type")]
-    public string ContentType { get; set; }
+    public string? ContentType { get; set; }
 
     [NotMapped]
     public IFormFile? Image { get; set; }
     
     // Navigation property
     public virtual BlogUser? Author { get; set; }
-    public virtual ICollection<Post> Posts { get; set; } = new HashSet<Post>();
+    public virtual ICollection<Post>? Posts { get; set; } = new HashSet<Post>();
 }
